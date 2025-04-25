22ac5693e5a6c563d69e44d5660dd5cba8521400
wycky1990 <wyclifffeomondi@gmail.com>
2025-04-26 02:54:11 +0400

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,6 @@
 ''' Module containing single function that adds two integers
 '''
 def add(a: float, b: float) -> float:
-'''addition of two integers
-'''
+    '''addition of two integers
+    '''
     return a + b
