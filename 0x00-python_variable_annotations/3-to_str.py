b0670058c19b826a4893a228088e9c6f18324191
wycky1990 <wyclifffeomondi@gmail.com>
2025-04-26 03:37:28 +0400

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -3,12 +3,8 @@
 '''
 
 
-def to_str(n: float) -> str:    
-    '''Returns a string representation of a float
-       Args:
-       n: A float to convert to a string
-       Returns:
-       A string representation of the float
+def to_str(n: float) -> str:
+    '''Returns a string representation of a float Args:n: A float to convert to a string. Returns:A string representation of the float.
     '''
 
     return str(n)
