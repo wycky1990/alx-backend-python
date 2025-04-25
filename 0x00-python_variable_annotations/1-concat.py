13a10c7524a6f117c78c0cb379201e35bd0354ff
wycky1990 <wyclifffeomondi@gmail.com>
2025-04-26 02:58:13 +0400

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 ## Contains a single function that concatenates two strings
 
+
 def concat(str1: str, str2: str) -> str:
-## Concatenates two strings
+    ## Concatenates two strings
 
     return str1 + str2
