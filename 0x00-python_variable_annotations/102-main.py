8d10080b75b8b72a95efbcd80f7e54b8e9757c2e
wycky1990 <wyclifffeomondi@gmail.com>
2025-04-26 04:32:47 +0400

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/102-main.py
+++ 0x00-python_variable_annotations/102-main.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+'''
+Module for zoom_array function
+'''
+
 
 zoom_array =  __import__('102-type_checking').zoom_array
+    '''
+    Returns a list with each item in the tuple repeated factor ty
+    '''
 
-print(zoom_array.__annotations__)
+    print(zoom_array.__annotations__)
 
