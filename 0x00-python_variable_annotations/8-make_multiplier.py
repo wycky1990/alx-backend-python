fd3968b7cf8b520a848c4edc666657a22adc3091
wycky1990 <wyclifffeomondi@gmail.com>
2025-04-26 04:18:38 +0400

    0x00. Python - Variable Annotations

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -8,7 +8,8 @@ from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     '''
-    Multiplies a float by multiplier Args:multiplier (float): The multiplier Returns:A function that multiplies a float by multiplier
+    Multiplies a float by multiplier Args:multiplier (float)
+
     '''
 
     def multiplier_func(number: float) -> float:
